7696a1e465122d6ff60ee7b7f75fdff196272757
taeilers <69173391+taeilers@users.noreply.github.com>
2022-03-15 23:23:37 -0400

    Before new class

--- main.cpp
+++ main.cpp
@@ -2,34 +2,62 @@
 
 using namespace sf;
 
+char chars[6][5];
+Text text[6][5];
+RectangleShape letterBox[6][5];
+
+void input(char chr)
+{
+    static int j = 0;
+    static int i = 0;
+
+    if (chr == '\b')
+    {
+
+    }
+    else
+    {
+        chars[i][j] = chr;
+        if (j > 5)
+        {
+            j = 0;
+            i++;
+        }
+        if (i > 6)
+        {
+            //end
+        }
+        j++;
+    }
+}
+
 int main()
 {
     RenderWindow window(VideoMode(400, 400), "Wordle");
-    
-    RectangleShape letterBox[5];
-    Text text[5];
+
     sf::Font font;
     font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
-    std::string str = "";
-
     
     float letterBoxPos[] = { 0.f, 10.f };
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 6; i++)
     {
-        text[i].setFillColor(sf::Color::White);
-        text[i].setFont(font);
-        text[i].setPosition(Vector2f(50.f, 50.f));
-        letterBoxPos[0] += 60.f;
-        letterBox[i].setPosition(letterBoxPos[0], letterBoxPos[1]);
-        letterBox[i].setSize(Vector2f(50.f, 50.f));
-        letterBox[i].setFillColor(Color::White);
+        for (int j = 0; j < 5; j++)
+        {
+            letterBoxPos[0] += 60.f;
+            letterBoxPos[1] *= i;
+            text[i][j].setFillColor(sf::Color::White);
+            text[i][j].setFont(font);
+            text[i][j].setPosition(Vector2f(75.f, 25.f));
+            letterBox[i][j].setPosition(letterBoxPos[0], letterBoxPos[1]);
+            letterBox[i][j].setSize(Vector2f(50.f, 50.f));
+            letterBox[i][j].setFillColor(Color::Blue);
+        }
     }
 
 
 
     while (window.isOpen())
     {
-        int k = 0;
         Event event;
         while (window.pollEvent(event))
         {
@@ -42,22 +70,20 @@ int main()
                 }
                 case Event::TextEntered:
                 {
-                    str += event.text.unicode;
-                    if (str.length() == 5)
-                    {
-                        text.setString(str);
-                        str = "";
-                        k++;
-                    }
+                    input(event.text.unicode);
                     break;
                 }
             }
         }
 
         window.clear();
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 6; i++)
         {
-            window.draw();
+            for (int j=0; j<5; j++)
+            {
+                //window.draw(letterBox[i][j]);
+                window.draw(text[i][j]);
+            }
         }
         window.display();
     }
